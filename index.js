95e2187f2a79c088ed0da16c287ad5fb0ecae5a9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-12-03 18:40:47 -0500

    Sorta working

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ let body = document.body;
 let styles = `
   .render-perf__container {
     position: fixed;
-    pointerEvents: none;
+    pointer-events: none;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
+    box-sizing: border-box;
     
     .render-perf__box {
+      box-sizing: border-box;
       font-size: 0.5rem;
       font-family: sans-serif;
+      display: block;
       position: fixed;
       pointer-events: none;
       border: 1px solid #aa00ff;
-      background-color: rgba(200, 0, 255, 0.1);
-      transition: opacity 1s;
+      background-color: rgba(200, 0, 255, 0.05);
+      transition: opacity 0.25s;
+
+      .render-perf__title {
+        margin-top: -1rem;
+      }
     }
   }
 `;
@@ -30,12 +37,15 @@ body.appendChild(overlay);
 
 class Highlight {
   #element;
+  #title;
   constructor(target) {
     this.target = target;
 
     this.#element = document.createElement('div');
+    this.#title = document.createElement('div');
     this.#element.classList.add('render-perf__box');
-    overlay.appendChild(this.#element);
+    this.#title.classList.add('render-perf__title');
+    this.#element.appendChild(this.#title);
   }
 
   #reasons = new Map();
@@ -62,28 +72,43 @@ class Highlight {
 
   get title() {
     let reasons = [...this.#reasons.entries()].map(([key, count]) => `${key} (${count})`).join(' , ');
-    return `${this.icon} Rerender | ${reasons}`;
+    let total = 0;
+
+    this.#reasons.values().forEach(x => total += x);
+
+    return `x${total} | Rerender | ${reasons}`;
   }
 
   #fadeOut;
+  #frame;
   #render() {
+    cancelAnimationFrame(this.#frame);
     clearTimeout(this.#fadeOut);
-    let rect = this.target.getBoundingClientRect();
+    this.#frame = requestAnimationFrame(() => {
+      let rect = this.target.getBoundingClientRect();
 
-    console.log(rect);
+      if (rect.top > window.innerHeight) return;
+      if (rect.left > window.innerWidth) return;
 
-    this.#element.textContent = this.title;
-    Object.assign(this.#element.style, {
-      opacity: 1,
-      top: rect.y + px,
-      left: rect.x + px,
-      width: rect.wdith + px,
-      height: rect.height + px,
-    });
+      if (!overlay.contains(this.#element)) {
+        overlay.appendChild(this.#element);
+      }
 
-    this.#fadeOut = setTimeout(() => {
-      this.#element.opacity = 0;
-    }, 500);
+      this.#title.textContent = this.title;
+      Object.assign(this.#element.style, {
+        top: rect.y + px,
+        left: rect.x + px,
+        // right: rect.right + px,
+        // bottom: rect.bottom + px,
+        width: rect.width + px,
+        height: rect.height + px,
+      });
+
+      this.#element.style.opacity = 1;
+      this.#fadeOut = setTimeout(() => {
+        this.#element.style.opacity = 0;
+      }, 1000);
+    })
   }
 }
 let cache = new WeakMap();
@@ -99,7 +124,6 @@ function highlightNode(element, why) {
 }
 
 let mutationObserver = new MutationObserver((mutationList, observer) => {
-  console.log(mutationList)
   for (let mutation of mutationList) {
     let shouldIgnore = mutation.target?.getAttribute('class')?.includes('render-perf__')
     if (shouldIgnore) {
@@ -125,5 +149,7 @@ mutationObserver.observe(body, {
   subtree: true,
   childList: true,
   attributes: true,
-  characterData: true,
+  //characterData: true,
 });
+
+console.log({ cache });
